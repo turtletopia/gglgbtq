31c6ea5e373431dcba57c5df485b4ea80147c2fe
Laura <erdaradun.gaztea+github@gmail.com>
2022-08-10 13:57:26 +0200

    collect pride data

---
+++ R/zzz_data.R
@@ -0,0 +1,56 @@
+pride_data <- list(
+  rainbow = list(
+    colors = c("#E40303", "#FF8C00", "#FFED00", "#008026", "#24408E", "#732982"),
+    theme = theme_lgbtq_white
+  ),
+  philadelphia = list(
+    colors = c("#000000", "#784F17", "#D12229", "#F68A1E", "#FDE01A", "#007940", "#24408E", "#732982"),
+    theme = theme_lgbtq_white
+  ),
+  progress = list(
+    colors = c("#D12229", "#F68A1E", "#FDE01A", "#007940", "#24408E", "#732982",
+               "#FFFFFF", "#FFAFC8", "#74D7EE", "#784F17", "#000000")
+  ),
+  bisexual = list(
+    colors = c("#D00070", "#8C4799", "#0032A0"),
+    theme = theme_bisexual
+  ),
+  asexual = list(
+    colors = c("#000000", "#A4A4A4", "#FFFFFF", "#810081")
+  ),
+  transgender = list(
+    colors = c("#55CDFC", "#F7A8B8", "#FFFFFF")
+  ),
+  pansexual = list(
+    colors = c("#FF1B8D", "#FFDA00", "#1BB3FF"),
+    theme = theme_lgbtq_white
+  ),
+  nonbinary = list(
+    colors = c("#FFF430", "#FFFFFF", "#9C59D1", "#000000")
+  ),
+  lesbian = list(
+    colors = c("#D62900", "#FF9B55", "#FFFFFF", "#D461A6", "#A50062")
+  ),
+  gay_man = list(
+    colors = c("#078D70", "#98E8C1", "#FFFFFF", "#7BADE2", "#3D1A78")
+  ),
+  intersex = list(
+    colors = c("#FFD800", "#7902AA"),
+    theme = theme_lgbtq_white
+  ),
+  aromantic = list(
+    colors = c("#3DA542", "#A7D379", "#FFFFFF", "#A4A4A4", "#000000")
+  ),
+  genderfluid = list(
+    colors = c("#FF76A4", "#FFFFFF", "#C011D7", "#000000", "#2F3CBE")
+  ),
+  genderqueer = list(
+    colors = c("#B57EDC", "#FFFFFF", "#4A8123")
+  ),
+  lesbian_7 = list(
+    colors = c("#A60061", "#B95594", "#D162A8", "#FFFFFF", "#E5ADD1", "#C64D53", "#8C1801")
+  ),
+  gay_man_7 = list(
+    colors = c("#078D70", "#26CEAA", "#98E8C1", "#FFFFFF", "#7BADE2", "#5049CC", "#3D1A78")
+  )
+)
