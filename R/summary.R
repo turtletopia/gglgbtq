ff5cd18de80b5e2429b17f3557c52f15065dde5d
Laura <erdaradun.gaztea+github@gmail.com>
2022-08-11 00:17:29 +0200

    implement show_pride()

---
+++ R/summary.R
@@ -0,0 +1,17 @@
+#' List available palettes
+#'
+#' @description Shows the names of available LGBTQ palettes and their metadata.
+#'
+#' @return A `data.frame` with palette names and numbers of colors.
+#'
+#' @examples
+#' show_pride()
+#'
+#' @export
+show_pride <- function() {
+  data.frame(
+    palettes = names(pride_data),
+    lengths = vapply(pride_data, function(p) length(p[["colors"]]),
+                     integer(1), USE.NAMES = FALSE)
+  )
+}
